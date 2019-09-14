69e7a347cf4fe2a63eee5a385cb9ac239b6e90b9
Evan Roman <evanroman1@gmail.com>
2019-09-14 11:36:13 -0400

    run autopep8

--- simplex.py
+++ simplex.py
@@ -1,7 +1,12 @@
-import ast, getopt, sys, copy, os
+import ast
+import getopt
+import sys
+import copy
+import os
 from fractions import Fraction
 
-clear = lambda: os.system('cls' if os.name == 'nt' else 'clear')
+
+def clear(): return os.system('cls' if os.name == 'nt' else 'clear')
 
 
 class SimplexSolver():
@@ -9,7 +14,7 @@ class SimplexSolver():
         output problem steps in LaTeX file.
     '''
 
-    # Table for converting inequality list to LaTeX    
+    # Table for converting inequality list to LaTeX
     latex_ineq = {'=': '=',
                   '<=': r'\leq',
                   '>=': r'\geq'}
@@ -34,12 +39,12 @@ class SimplexSolver():
         self.gen_doc = latex
         self.ineq = ineq
 
-        # Create the header for the latex doc.        
+        # Create the header for the latex doc.
         self.start_doc()
 
         # Add slack & artificial variables
         self.set_simplex_input(A, b, c)
-            
+
         # Are there any negative elements on the bottom (disregarding
         # right-most element...)
         while (not self.should_terminate()):
@@ -48,15 +53,15 @@ class SimplexSolver():
                 clear()
                 self._print_tableau()
                 print(("Current solution: %s\n" %
-                      str(self.get_current_solution())))
+                       str(self.get_current_solution())))
                 self._prompt()
-            
+
             # Attempt to find a non-negative pivot.
             pivot = self.find_pivot()
             if pivot[1] < 0:
                 if (enable_msg):
-                    print ("There exists no non-negative pivot. "
-                           "Thus, the solution is infeasible.")
+                    print("There exists no non-negative pivot. "
+                          "Thus, the solution is infeasible.")
                 self.infeasible_doc()
                 self.print_doc()
                 return None
@@ -66,12 +71,12 @@ class SimplexSolver():
                     clear()
                     self._print_tableau()
                     print(("\nThere are negative elements in the bottom row, "
-                          "so the current solution is not optimal. "
-                          "Thus, pivot to improve the current solution. The "
-                          "entering variable is %s and the departing "
-                          "variable is %s.\n" %
+                           "so the current solution is not optimal. "
+                           "Thus, pivot to improve the current solution. The "
+                           "entering variable is %s and the departing "
+                           "variable is %s.\n" %
                            (str(self.entering[pivot[0]]),
-                           str(self.departing[pivot[1]]))))
+                            str(self.departing[pivot[1]]))))
                     self._prompt()
                     print("\nPerform elementary row operations until the "
                           "pivot is one and all other elements in the "
@@ -90,13 +95,13 @@ class SimplexSolver():
             print("That's all folks!")
         self.print_doc()
         return solution
-        
+
     def set_simplex_input(self, A, b, c):
         ''' Set initial variables and create tableau.
         '''
         # Convert all entries to fractions for readability.
         for a in A:
-            self.A.append([Fraction(x) for x in a])    
+            self.A.append([Fraction(x) for x in a])
         self.b = [Fraction(x) for x in b]
         self.c = [Fraction(x) for x in c]
         if not self.ineq:
@@ -104,7 +109,7 @@ class SimplexSolver():
                 self.ineq = ['<='] * len(b)
             elif self.prob == 'min':
                 self.ineq = ['>='] * len(b)
-            
+
         self.update_enter_depart(self.get_Ab())
         self.init_problem_doc()
 
@@ -113,10 +118,10 @@ class SimplexSolver():
             # ... find the dual maximum and solve that.
             m = self.get_Ab()
             m.append(self.c + [0])
-            m = [list(t) for t in zip(*m)] # Calculates the transpose
+            m = [list(t) for t in zip(*m)]  # Calculates the transpose
             self.A = [x[:(len(x)-1)] for x in m]
             self.b = [y[len(y) - 1] for y in m]
-            self.c = m[len(m) -1]
+            self.c = m[len(m) - 1]
             self.A.pop()
             self.b.pop()
             self.c.pop()
@@ -171,21 +176,21 @@ class SimplexSolver():
     def pivot(self, pivot_index):
         ''' Perform operations on pivot.
         '''
-        j,i = pivot_index
+        j, i = pivot_index
 
         pivot = self.tableau[i][j]
         self.tableau[i] = [element / pivot for
                            element in self.tableau[i]]
         for index, row in enumerate(self.tableau):
-           if index != i:
-              row_scale = [y * self.tableau[index][j]
-                          for y in self.tableau[i]]
-              self.tableau[index] = [x - y for x,y in
-                                     zip(self.tableau[index],
-                                         row_scale)]
+            if index != i:
+                row_scale = [y * self.tableau[index][j]
+                             for y in self.tableau[i]]
+                self.tableau[index] = [x - y for x, y in
+                                       zip(self.tableau[index],
+                                           row_scale)]
 
         self.departing[i] = self.entering[j]
-        
+
     def get_entering_var(self):
         ''' Get entering variable by determining the 'most negative'
             element of the bottom row.
@@ -198,7 +203,6 @@ class SimplexSolver():
                 most_neg = value
                 most_neg_ind = index
         return most_neg_ind
-            
 
     def get_departing_var(self, entering_index):
         ''' To calculate the departing variable, get the minimum of the ratio
@@ -213,7 +217,7 @@ class SimplexSolver():
                 min_ratio_index = index
                 min_ratio = x[len(x)-1]/x[entering_index]
                 break
-        
+
         if min_ratio > 0:
             for index, x in enumerate(self.tableau):
                 if index > skip and x[entering_index] > 0:
@@ -221,7 +225,7 @@ class SimplexSolver():
                     if min_ratio > ratio:
                         min_ratio = ratio
                         min_ratio_index = index
-        
+
         return min_ratio_index
 
     def get_Ab(self):
@@ -250,13 +254,13 @@ class SimplexSolver():
         for x in self.entering:
             if x is not 'b':
                 if x in self.departing:
-                    solution[x] = self.tableau[self.departing.index(x)]\
-                                  [len(self.tableau[self.departing.index(x)])-1]
+                    solution[x] = self.tableau[self.departing.index(
+                        x)][len(self.tableau[self.departing.index(x)])-1]
                 else:
                     solution[x] = 0
-        solution['z'] = self.tableau[len(self.tableau) - 1]\
-                          [len(self.tableau[0]) - 1]
-        
+        solution['z'] = self.tableau[len(
+            self.tableau) - 1][len(self.tableau[0]) - 1]
+
         # If this is a minimization problem...
         if (self.prob == 'min'):
             # ... then get x_1, ..., x_n  from last element of
@@ -264,7 +268,8 @@ class SimplexSolver():
             bottom_row = self.tableau[len(self.tableau) - 1]
             for v in self.entering:
                 if 's' in v:
-                    solution[v.replace('s', 'x')] = bottom_row[self.entering.index(v)]    
+                    solution[v.replace('s', 'x')
+                             ] = bottom_row[self.entering.index(v)]
 
         return solution
 
@@ -301,7 +306,7 @@ class SimplexSolver():
                 opp = ''
             if x == 1 or x == -1:
                 x = ''
-            func += (r"%s %sx_%s "  % (opp, str(x), str(index+1)))
+            func += (r"%s %sx_%s " % (opp, str(x), str(index+1)))
             found_value = True
         self.doc += (r"\max{%s} \\ "
                      r"\end{equation*}" % func)
@@ -329,17 +334,18 @@ class SimplexSolver():
                 if index != len(matrix[i]) - 1:
                     if x == 1 or x == -1:
                         x = ''
-                    self.doc += (r"%s %s%s "  % (opp, str(x),
-                                                 str(self.entering[index])))
+                    self.doc += (r"%s %s%s " % (opp, str(x),
+                                                str(self.entering[index])))
                 else:
-                    self.doc += (r"%s %s"  % (self.latex_ineq[self.ineq[i]],str(x)))
+                    self.doc += (r"%s %s" %
+                                 (self.latex_ineq[self.ineq[i]], str(x)))
                 found_value = True
                 if (index == len(matrix[i]) - 1):
-                    self.doc += r" \\ "        
+                    self.doc += r" \\ "
         self.doc += (r"\end{array}"
                      r"\right."
                      r"\]")
- 
+
     def slack_doc(self):
         if not self.gen_doc:
             return
@@ -356,7 +362,7 @@ class SimplexSolver():
                      r"Create the initial tableau of the new linear system."
                      r"\end{flushleft}")
         self.tableau_doc()
-            
+
     def tableau_doc(self):
         if not self.gen_doc:
             return
@@ -405,18 +411,18 @@ class SimplexSolver():
                      r"variable is $%s$."
                      r"\end{flushleft}" %
                      (str(self.entering[pivot[0]]),
-                     str(self.departing[pivot[1]])))
+                      str(self.departing[pivot[1]])))
         self.doc += (r"\begin{flushleft}"
                      r"Perform elementary row operations until the "
                      r"pivot element is 1 and all other elements in the "
                      r"entering column are 0."
                      r"\end{flushleft}")
-    
+
     def current_solution_doc(self, solution):
         if not self.gen_doc:
             return
         self.doc += r"\begin{equation*}"
-        for key,value in sorted(solution.items()):
+        for key, value in sorted(solution.items()):
             self.doc += r"%s = %s" % (key, self._fraction_to_latex(value))
             if key != 'z':
                 self.doc += r", "
@@ -457,7 +463,7 @@ class SimplexSolver():
                     row.append(0)
             I.append(row)
         return I
-        
+
     def _print_matrix(self, M):
         ''' Print some matrix.
         '''
@@ -478,13 +484,14 @@ class SimplexSolver():
             print('|', end=' ')
             for index, val in enumerate(row):
                 print('{:^5}'.format(str(val)), end=' ')
-            if num < (len(self.tableau) -1):
+            if num < (len(self.tableau) - 1):
                 print('| %s' % self.departing[num])
             else:
                 print('|')
 
     def _prompt(self):
         input("Press enter to continue...")
+
 
 if __name__ == '__main__':
     clear()
@@ -494,9 +501,9 @@ if __name__ == '__main__':
     b = []
     c = []
     p = ''
-    argv = sys.argv[1:]    
+    argv = sys.argv[1:]
     try:
-        opts, args = getopt.getopt(argv,"hA:b:c:p:",["A=","b=","c=","p="])
+        opts, args = getopt.getopt(argv, "hA:b:c:p:", ["A=", "b=", "c=", "p="])
     except getopt.GetoptError:
         print('simplex.py -A <matrix> -b <vector> -c <vector> -p <type>')
         sys.exit(2)
@@ -524,5 +531,5 @@ if __name__ == '__main__':
     # Assume maximization problem as default.
     if p not in ('max', 'min'):
         p = 'max'
-    
-    SimplexSolver().run_simplex(A,b,c,prob=p,enable_msg=False,latex=True)
+
+    SimplexSolver().run_simplex(A, b, c, prob=p, enable_msg=False, latex=True)
--- simplex_test.py
+++ simplex_test.py
@@ -6,6 +6,7 @@ import unittest
 class SimplexSolverTest(unittest.TestCase):
 
     ''' STANDARD FORM MAXIMIZATION EXAMPLES '''
+
     def test_max_feasible1(self):
         ''' Find optimal solution to max(x1 + x2) such that:
             Ax <= b,
@@ -15,15 +16,15 @@ class SimplexSolverTest(unittest.TestCase):
 
             Source: http://mat.gsia.cmu.edu/classes/QUANT/NOTES/chap7.pdf, Example 7.2.1
         '''
-        self.assertDictEqual({'x_1': Fraction(5,3),
-                              'x_2': Fraction(2,3),
+        self.assertDictEqual({'x_1': Fraction(5, 3),
+                              'x_2': Fraction(2, 3),
                               's_1': Fraction(0),
                               's_2': Fraction(0),
-                              'z': Fraction(7,3)},
-                              SimplexSolver().run_simplex([[2,1],
-                                                           [1,2]],
-                                                          [4,3],
-                                                          [1,1]))
+                              'z': Fraction(7, 3)},
+                             SimplexSolver().run_simplex([[2, 1],
+                                                          [1, 2]],
+                                                         [4, 3],
+                                                         [1, 1]))
 
     def test_max_feasible2(self):
         ''' Find optimal solution to max(4x1 + 6x2) such that:
@@ -41,11 +42,11 @@ class SimplexSolverTest(unittest.TestCase):
                               's_2': Fraction(0),
                               's_3': Fraction(0),
                               'z': Fraction(132)},
-                              SimplexSolver().run_simplex([[-1,1],
-                                                           [1,1],
-                                                           [2,5]],
-                                                          [11,27,90],
-                                                          [4,6]))
+                             SimplexSolver().run_simplex([[-1, 1],
+                                                          [1, 1],
+                                                          [2, 5]],
+                                                         [11, 27, 90],
+                                                         [4, 6]))
 
     def test_max_feasible3(self):
         ''' Find optimal solution to max(2x1 - x2 + 2x3) such that:
@@ -59,16 +60,16 @@ class SimplexSolverTest(unittest.TestCase):
         '''
         self.assertDictEqual({'x_1': Fraction(5),
                               'x_2': Fraction(0),
-                              'x_3': Fraction(5,2),
+                              'x_3': Fraction(5, 2),
                               's_1': Fraction(0),
                               's_2': Fraction(20),
                               's_3': Fraction(0),
                               'z': Fraction(15)},
-                              SimplexSolver().run_simplex([[2,1,0],
-                                                           [1,2,-2],
-                                                           [0,1,2]],
-                                                          [10,20,5],
-                                                          [2,-1,2]))
+                             SimplexSolver().run_simplex([[2, 1, 0],
+                                                          [1, 2, -2],
+                                                          [0, 1, 2]],
+                                                         [10, 20, 5],
+                                                         [2, -1, 2]))
 
     def test_max_feasible4(self):
         ''' Find optimal solution to max(2x1 - x2 + 2x3) such that:
@@ -87,11 +88,11 @@ class SimplexSolverTest(unittest.TestCase):
                               's_2': Fraction(0),
                               's_3': Fraction(1),
                               'z': Fraction(45)},
-                              SimplexSolver().run_simplex([[4,1,1],
-                                                           [2,3,1],
-                                                           [1,2,3]],
-                                                          [30,60,40],
-                                                          [3,2,1]))
+                             SimplexSolver().run_simplex([[4, 1, 1],
+                                                          [2, 3, 1],
+                                                          [1, 2, 3]],
+                                                         [30, 60, 40],
+                                                         [3, 2, 1]))
 
     def test_max_feasible5(self):
         ''' Find optimal solution to max(11x1 + 16x2 + 15x3) such that:
@@ -110,14 +111,14 @@ class SimplexSolverTest(unittest.TestCase):
                               's_2': Fraction(0),
                               's_3': Fraction(0),
                               'z': Fraction(100200)},
-                              SimplexSolver().run_simplex([[1,2,Fraction(3,2)],
-                                                           [Fraction(2,3),
-                                                            Fraction(2,3),1],
-                                                           [Fraction(1,2),
-                                                            Fraction(1,3),
-                                                            Fraction(1,2)]],
-                                                          [12000,4600,2400],
-                                                          [11,16,15]))
+                             SimplexSolver().run_simplex([[1, 2, Fraction(3, 2)],
+                                                          [Fraction(2, 3),
+                                                           Fraction(2, 3), 1],
+                                                          [Fraction(1, 2),
+                                                           Fraction(1, 3),
+                                                           Fraction(1, 2)]],
+                                                         [12000, 4600, 2400],
+                                                         [11, 16, 15]))
 
     def test_max_feasible6(self):
         ''' Find optimal solution to max(11x1 + 16x2 + 15x3) such that:
@@ -138,15 +139,16 @@ class SimplexSolverTest(unittest.TestCase):
                               's_3': Fraction(0),
                               's_4': Fraction(12),
                               'z': Fraction(1052000)},
-                              SimplexSolver().run_simplex([[20,6,3],
-                                                           [0,1,0],
-                                                           [-1,-1,1],
-                                                           [-9,1,1]],
-                                                          [182,10,0,0],
-                                                          [100000,
-                                                           40000,
-                                                           18000]))
+                             SimplexSolver().run_simplex([[20, 6, 3],
+                                                          [0, 1, 0],
+                                                          [-1, -1, 1],
+                                                          [-9, 1, 1]],
+                                                         [182, 10, 0, 0],
+                                                         [100000,
+                                                          40000,
+                                                          18000]))
     ''' STANDARD FORM MINIMIZATION PROBLEMS '''
+
     def test_min_feasible1(self):
         ''' Find optimal solution to min(3x1 + 2x2) such that:         
             Ax >= b,
@@ -163,11 +165,11 @@ class SimplexSolverTest(unittest.TestCase):
                               'y_1': Fraction(1),
                               'y_2': Fraction(1),
                               'z': Fraction(10)},
-                              SimplexSolver().run_simplex([[2,1],
-                                                           [1,1]],
-                                                          [6,4],
-                                                          [3,2],
-                                                          prob='min'))
+                             SimplexSolver().run_simplex([[2, 1],
+                                                          [1, 1]],
+                                                         [6, 4],
+                                                         [3, 2],
+                                                         prob='min'))
 
     def test_min_feasible2(self):
         ''' Find optimal solution to min(2x1 + 10x2 + 8x3) such that:
@@ -189,12 +191,12 @@ class SimplexSolverTest(unittest.TestCase):
                               'y_2': Fraction(3),
                               'y_3': Fraction(0),
                               'z': Fraction(36)},
-                              SimplexSolver().run_simplex([[1,1,1],
-                                                           [0,1,2],
-                                                           [-1,2,2]],
-                                                          [6,8,4],
-                                                          [2,10,8],
-                                                          prob='min'))
+                             SimplexSolver().run_simplex([[1, 1, 1],
+                                                          [0, 1, 2],
+                                                          [-1, 2, 2]],
+                                                         [6, 8, 4],
+                                                         [2, 10, 8],
+                                                         prob='min'))
 
     def test_min_feasible3(self):
         ''' Find optimal solution to min(20000x1 + 25000x2) such that:
@@ -210,17 +212,19 @@ class SimplexSolverTest(unittest.TestCase):
                               'x_2': Fraction(50),
                               's_1': Fraction(0),
                               's_2': Fraction(0),
-                              'y_1': Fraction(250,7),
+                              'y_1': Fraction(250, 7),
                               'y_2': Fraction(0),
                               'y_3': Fraction(200, 7),
                               'z': Fraction(1750000)},
-                              SimplexSolver().run_simplex([[400,300],
-                                                           [300,400],
-                                                           [200,500]],
-                                                          [25000,
-                                                           27000,
-                                                           30000],
-                                                          [20000,25000],
-                                                          prob='min'))
+                             SimplexSolver().run_simplex([[400, 300],
+                                                          [300, 400],
+                                                          [200, 500]],
+                                                         [25000,
+                                                          27000,
+                                                          30000],
+                                                         [20000, 25000],
+                                                         prob='min'))
+
+
 if __name__ == '__main__':
     unittest.main()
